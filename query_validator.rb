ee6ad0a7f9b274e58cd219a492702fd520d37459
Nathaniel Woodthorpe <njwoodthorpe@gmail.com>
2017-08-24 11:20:13 -0400

    Hide Schema type, validator is now an object instead of a singleton class

--- query_validator.rb
+++ query_validator.rb
@@ -1,35 +1,39 @@
 require './contexts/context'
 require './contexts/root_context'
+require './schema'
 require './query_tokenizer'
 require './query_ast'
 
 class QueryValidator
   class ValidationException < Exception; end
 
-  class << self
-    def validate(schema, query)
-      @schema = schema
-      tokenizer = QueryTokenizer.new(query)
-      ast = QueryAST.build(tokenizer)
+  def initialize(schema)
+    @schema = Schema.new(schema)
+  end
 
-      validate_node(ast, RootContext.new)
+  def validate(query)
+    tokenizer = QueryTokenizer.new(query)
+    ast = QueryAST.build(tokenizer)
+
+    validate_node(ast, RootContext.new)
+  end
+
+  private
+
+  def validate_node(ast, parent_context)
+    # first, check if field is valid in parent context
+    # then check if args are valid
+    # then recursively validate body nodes
+    parent_context.validate_field_present(ast[:field], @schema)
+
+    ast[:arguments].each do |key, value|
+      parent_context.validate_field_arg(ast[:field], key, value, @schema)
     end
 
-    def validate_node(ast, parent_context)
-      # first, check if field is valid in parent context
-      # then check if args are valid
-      # then recursively validate body nodes
-      parent_context.validate_field_present(ast[:field], @schema)
+    ast_context = parent_context.get_context_for_field(ast[:field], @schema) || return
 
-      ast[:arguments].each do |key, value|
-        parent_context.validate_field_arg(ast[:field], key, value, @schema)
-      end
-
-      ast_context = parent_context.get_context_for_field(ast[:field], @schema) || return
-
-      ast[:body].each do |child_ast|
-        validate_node(child_ast, ast_context)
-      end
+    ast[:body].each do |child_ast|
+      validate_node(child_ast, ast_context)
     end
   end
 end
